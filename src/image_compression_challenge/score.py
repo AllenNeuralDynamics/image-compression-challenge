895b77285db884220518fa1fe26beb6f89007fda
anna-grim <anna.grim@alleninstitute.org>
2025-09-27 00:35:57 +0000

    refactor: updates to segmentation eval

--- src/image_compression_challenge/score.py
+++ src/image_compression_challenge/score.py
@@ -11,46 +11,59 @@ Code that scores submissions to the image compression data challenge.
 from segmentation_skeleton_metrics.skeleton_metric import SkeletonMetric
 from segmentation_skeleton_metrics.utils.img_util import TiffReader
 
+import os
+import shutil
+
 from image_compression_challenge import utils
 
 
 def score(zip_path, read_fn):
-    # Check submission contains required files
-    check_files_in_submission(zip_path)
-
-    # Check segmentation consistency
-    if is_segmentation_consistent(zip_path):
-        print("Pass [2/2]: Submission has consistent segmentation!")
-    else:
-        print("Failed Segmentation Consistency Check!")
-        return np.inf
+    # Check submission is valid
+    check_required_submission_files(zip_path)
+    check_segmentation_consistency(zip_path)
 
     # Score submission
     compression_score = compute_compressed_size(zip_path)
     decompression_score = compute_decompression_cost(zip_path, read_fn)
+    score = compression_score + compression_score
+    return score
 
 
-def check_files_in_submission(zip_path):
+# --- Check Submission ---
+def check_required_submission_files(zip_path):
+    """
+    Checks if a participant's submission contains the required image,
+    segmentation, and SWC files.
+    """
     for n in range(5, 6):
+        #img_filename = f"block_00{n}"
         segmentation_filename = f"segmentation_00{n}.tiff"
         skeletons_filename = f"skeletons_00{n}.zip"
+        #assert utils.is_file_in_zip(zip_path, img_filename)
         assert utils.is_file_in_zip(zip_path, segmentation_filename)
         assert utils.is_file_in_zip(zip_path, skeletons_filename)
     print("Pass [1/2]: Submission contains required files!")
 
-# --- Check Segmentations ---
-def is_segmentation_consistent(zip_path):
+
+def check_segmentation_consistency(zip_path):
+    # Initializations
+    move_skeleton_zips(zip_path)
+
+    # Evaluation
     for n in range(5, 6):
         block_num = f"00{n}"
         compute_segmentation_metrics(zip_path, n)
+
+    utils.rmdir("./temp")
+    print("Pass [2/2]: Submission has consistent segmentation!")
 
 
 def compute_segmentation_metrics(zip_path, n):
     # Paths
     gt_path = f"s3://aind-benchmark-data/3d-image-compression/swcs/block_00{n}/"
-    output_dir = "./temp"
     segmentation_filename = f"submission/segmentation_00{n}.tiff"
-    skeletons_path = f"{zip_path}/skeletons_00{n}.zip"
+    skeletons_path = f"./temp/skeletons_00{n}.zip"
+    output_dir = "./temp"
 
     # Read segmentation
     segmentation = TiffReader(
@@ -64,6 +77,29 @@ def compute_segmentation_metrics(zip_path, n):
         gt_path, segmentation, output_dir, fragments_pointer=skeletons_path
     )
     skeleton_metric.run()
+
+
+def move_skeleton_zips(zip_path):
+    """
+    Extracts specific skeleton ZIP archives from a parent ZIP file and moves
+    them into a temporary directory. This extraction ensures that the SWC
+    files are stored in a format compatible with the SWC readers used in
+    segmentation-skeleton-metrics.
+
+    Parameters
+    ----------
+    zip : str
+        Path to a participant's submission ZIP archive.
+    """
+    # Initialize temp directory
+    output_dir = "./temp"
+    utils.mkdir(output_dir)
+
+    # Iterate over skeletons
+    for n in range(5, 6):
+        source_filename = f"skeletons_00{n}.zip"
+        destination_path = f"{output_dir}/skeletons_00{n}.zip"
+        utils.move_zip_in_zip(zip_path, source_filename, destination_path)
 
 
 # --- Compute Score ---
--- src/image_compression_challenge/utils.py
+++ src/image_compression_challenge/utils.py
@@ -14,6 +14,66 @@ import tifffile
 import zipfile
 
 
+# --- OS utils ---
+def mkdir(path, delete=False):
+    """
+    Creates a directory at the given path.
+
+    Parameters
+    ----------
+    path : str
+        Path of directory to be created.
+    delete : bool, optional
+        Indication of whether to delete directory at path if it already
+        exists. The default is False.
+    """
+    if delete:
+        rmdir(path)
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
+def rmdir(path):
+    """
+    Removes the given directory and all of its subdirectories.
+
+    Parameters
+    ----------
+    path : str
+        Path to directory to be removed if it exists.
+    """
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+
+# --- ZIP utils ---
+def move_zip_in_zip(outer_zip_path, inner_zip_name, output_path):
+    """
+    Extracts a nested ZIP file from within a parent ZIP archive and saves it
+    to a specified output path.
+
+    Parameters
+    ----------
+    outer_zip_path : str
+        Path to the parent ZIP file containing the nested ZIP.
+    inner_zip_name : str
+        Name (or suffix) of the inner ZIP file to extract.
+    output_path : str
+        Destination path where the extracted inner ZIP should be saved.
+    """
+    with zipfile.ZipFile(outer_zip_path, 'r') as outer_zip:
+        # Find file path
+        matches = [f for f in outer_zip.namelist() if f.endswith(inner_zip_name)]
+        if not matches:
+            raise FileNotFoundError(f"{self.inner_tiff} not found in ZIP")
+        filename = matches[0]
+
+        # Move file
+        inner_zip_bytes = outer_zip.read(filename)
+        with open(output_path, 'wb') as f_out:
+            f_out.write(inner_zip_bytes)
+
+
 def is_file_in_zip(zip_path, filename):
     """
     Checks if the given filename is contained in the ZIP archive.
