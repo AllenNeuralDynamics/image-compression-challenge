76472708779dd719092a3a17aaf3782a5e0ae717
anna-grim <anna.grim@alleninstitute.org>
2025-10-02 03:00:34 +0000

    refactor: improved segmentation consistency check

--- src/image_compression_challenge/score.py
+++ src/image_compression_challenge/score.py
@@ -12,9 +12,12 @@ from segmentation_skeleton_metrics.skeleton_metric import SkeletonMetric
 from segmentation_skeleton_metrics.utils.img_util import TiffReader
 
 import numpy as np
+import pandas as pd
 import shutil
 
 from image_compression_challenge import utils
+
+BLOCK_NUMS = ["005", "006", "007", "008", "009"]
 
 
 def score(zip_path, read_fn):
@@ -25,7 +28,7 @@ def score(zip_path, read_fn):
     # Score submission
     compression_score = compute_compressed_size(zip_path)
     decompression_score = compute_decompression_cost(zip_path, read_fn)
-    score = compression_score + compression_score
+    score = compression_score + decompression_score
     return score
 
 
@@ -41,10 +44,10 @@ def check_required_submission_files(zip_path):
         assert utils.is_file_in_zip(zip_path, filename), err_msg
 
     # Main
-    for n in range(5, 10):
-        #check_file(f"block_00{n}")
-        check_file(f"segmentation_00{n}.tiff")
-        check_file(f"skeletons_00{n}.zip")
+    for num in BLOCK_NUMS:
+        #check_file(f"block_{num}")
+        check_file(f"segmentation_{num}.tiff")
+        check_file(f"skeletons_{num}.zip")
     print("Pass [1/2]: Submission contains required files!")
 
 
@@ -53,28 +56,52 @@ def check_segmentation_consistency(zip_path):
     move_skeleton_zips(zip_path)
 
     # Evaluation
-    for n in range(5, 10):
-        compute_segmentation_metrics(zip_path, n)
+    for num in BLOCK_NUMS:
+        # Load segmentation results
+        baseline_result = load_baseline_segmentation_result(num)
+        submission_result = compute_segmentation_metrics(zip_path, num)
+
+        # Compare segmentation results
 
     #utils.rmdir("./temp")
     print("Pass [2/2]: Submission has consistent segmentation!")
 
 
-def compute_segmentation_metrics(zip_path, n):
+# --- Compute Score ---
+def compute_compressed_size(zip_path):
+    return np.inf
+
+
+def compute_decompression_cost(zip_path, read_fn):
+    return np.inf
+
+
+# --- Helpers ---
+def compute_segmentation_metrics(zip_path, num):
     # Paths
-    gt_path = f"s3://aind-benchmark-data/3d-image-compression/swcs/block_00{n}/"
-    segmentation_filename = f"segmentation_00{n}.tiff"
-    skeletons_path = f"./temp/skeletons_00{n}.zip"
-    output_dir = f"./temp/block_00{n}"
+    gt_path = f"s3://aind-benchmark-data/3d-image-compression/swcs/block_{num}/"
+    segmentation_filename = f"segmentation_{num}.tiff"
+    skeletons_path = f"./temp/skeletons_{num}.zip"
+    output_dir = f"./temp/block_{num}"
 
     # Read segmentation
     segmentation = TiffReader(zip_path, inner_tiff=segmentation_filename)
 
     # Run evaluation
     skeleton_metric = SkeletonMetric(
-        gt_path, segmentation, output_dir, fragments_pointer=skeletons_path
+        gt_path,
+        segmentation,
+        output_dir,
+        fragments_pointer=skeletons_path,
+        anisotropy=(0.748, 0.748, 1.0),
     )
     skeleton_metric.run()
+    return None  #pd.read_csv("./temp/results.csv")
+
+
+def load_baseline_segmentation_result(num):
+    url = f"https://raw.githubusercontent.com/AllenNeuralDynamics/image-compression-challenge/main/baseline_segmentation_results/results_{num}.csv"
+    return pd.read_csv(url)
 
 
 def move_skeleton_zips(zip_path):
@@ -94,16 +121,7 @@ def move_skeleton_zips(zip_path):
     utils.mkdir(output_dir)
 
     # Iterate over skeletons
-    for n in range(5, 10):
-        source_filename = f"skeletons_00{n}.zip"
-        destination_path = f"{output_dir}/skeletons_00{n}.zip"
+    for num in BLOCK_NUMS:
+        source_filename = f"skeletons_{num}.zip"
+        destination_path = f"{output_dir}/skeletons_{num}.zip"
         utils.move_zip_in_zip(zip_path, source_filename, destination_path)
-
-
-# --- Compute Score ---
-def compute_compressed_size(zip_path):
-    return np.inf
-
-
-def compute_decompression_cost(zip_path, read_fn):
-    return np.inf
