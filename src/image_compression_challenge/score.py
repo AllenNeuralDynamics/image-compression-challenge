3ad2cf94031dfd431f3f610a2e6b9b5162245f67
anna-grim <anna.grim@alleninstitute.org>
2025-10-08 17:43:13 +0000

    feat: compare segmentation results

--- src/image_compression_challenge/score.py
+++ src/image_compression_challenge/score.py
@@ -11,6 +11,7 @@ Code that scores submissions to the image compression data challenge.
 from segmentation_skeleton_metrics.skeleton_metric import SkeletonMetric
 from segmentation_skeleton_metrics.utils.img_util import TiffReader
 from time import time
+from tqdm import tqdm
 
 import numpy as np
 import pandas as pd
@@ -19,14 +20,21 @@ import shutil
 from image_compression_challenge import utils
 
 BLOCK_NUMS = ["005", "006", "007", "008", "009"]
+ERROR_TOLS = {
+    "% Omit Edges": 10,
+    "Split Rate": 500,
+    "Merge Rate": 500
+}
 
 
 def score(zip_path, read_fn):
     # Check submission is valid
+    print("Check Submission...")
     check_required_submission_files(zip_path)
     check_segmentation_consistency(zip_path)
 
     # Score submission
+    print("Score Submission...")
     compression_score = compute_compressed_size(zip_path)
     decompression_score = compute_decompression_cost(zip_path, read_fn)
     return compression_score, decompression_score
@@ -35,8 +43,13 @@ def score(zip_path, read_fn):
 # --- Check Submission ---
 def check_required_submission_files(zip_path):
     """
-    Checks if a participant's submission contains the required image,
-    segmentation, and SWC files.
+    Checks if a participant's submission contains the required compressed
+    image, segmentation, and SWC files.
+
+    Parameters
+    ----------
+    zip_path : str
+        Path to a participant's submission ZIP archive.
     """
     # Subroutines
     def check_file(filename):
@@ -44,11 +57,10 @@ def check_required_submission_files(zip_path):
         assert utils.is_file_in_zip(zip_path, filename), err_msg
 
     # Main
-    for num in BLOCK_NUMS:
+    for num in tqdm(BLOCK_NUMS, desc="Checking Required Files"):
         #check_file(f"compressed_{num}")
         check_file(f"segmentation_{num}.tiff")
         check_file(f"skeletons_{num}.zip")
-    print("Pass [1/2]: Submission contains required files!")
 
 
 def check_segmentation_consistency(zip_path):
@@ -62,9 +74,11 @@ def check_segmentation_consistency(zip_path):
         submission_result = compute_segmentation_metrics(zip_path, num)
 
         # Compare segmentation results
-
+        for metric in tqdm(ERROR_TOLS, desc="Checking Segmentation"):
+            error = submission_result[metric] - baseline_result[metric]
+            if error > ERROR_TOLS[metric]:
+                raise ValueError(f"Failed with {metric}={submission_result[metric]}")
     #utils.rmdir("./temp")
-    print("Pass [2/2]: Submission has consistent segmentation!")
 
 
 # --- Compute Score ---
@@ -91,7 +105,7 @@ def compute_segmentation_metrics(zip_path, num):
     gt_path = f"s3://aind-benchmark-data/3d-image-compression/swcs/block_{num}/"
     segmentation_filename = f"segmentation_{num}.tiff"
     skeletons_path = f"./temp/skeletons_{num}.zip"
-    output_dir = "./temp/block"
+    output_dir = "./temp"
 
     # Read segmentation
     segmentation = TiffReader(zip_path, inner_tiff=segmentation_filename)
@@ -105,7 +119,16 @@ def compute_segmentation_metrics(zip_path, num):
         anisotropy=(0.748, 0.748, 1.0),
     )
     skeleton_metric.run()
-    return pd.read_csv("./temp/results.csv")
+
+    # Process result
+    result = pd.read_csv("./temp/results.csv")
+    return fill_nan_results(result)
+
+
+def fill_nan_results(df):
+    df["Merge Rate"] = df["Merge Rate"].fillna(df["SWC Run Length"])
+    df["Split Rate"] = df["Split Rate"].fillna(df["SWC Run Length"])
+    return df
 
 
 def find_compressed_path(zip_path, num):
@@ -117,7 +140,7 @@ def find_compressed_path(zip_path, num):
     zip_path : str
         Path to a participant's submission ZIP archive.
     num : str
-        Unique identifier for an image block. 
+        Unique identifier for an image block.
     """
     pass
 
@@ -138,7 +161,8 @@ def load_baseline_segmentation_result(num):
         Skeleton-based metric results for the baseline segmentation.
     """
     url = f"https://raw.githubusercontent.com/AllenNeuralDynamics/image-compression-challenge/main/baseline_segmentation_results/results_{num}.csv"
-    return pd.read_csv(url)
+    result = pd.read_csv(url)
+    return fill_nan_results(result)
 
 
 def move_skeleton_zips(zip_path):
